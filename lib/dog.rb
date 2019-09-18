a9f089da27965ce3494814dbff69b8bbecb55f64
KG ZZ <czheng5h88@gmail.com>
2019-09-18 18:09:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Your code goes here!
 class Dog 
-  def name = (dog_name)
-    @this_name = dog_name 
-  end 
+  def name=(dogs_name)
+    @this_name = dogs_name
+  end
   def name 
     @this_name
   end 
